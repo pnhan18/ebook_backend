35318f761781f0a5e8d5a55ad508725cf1bf8a9c
Phan Van Nhan <pnhan0195@gmail.com>
2025-11-25 18:05:42 +0700

    Implement change password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,10 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards';
+import { type AuthenticatedUser } from 'src/common';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import { CurrentUser } from 'src/common/decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +25,14 @@ export class AuthController {
             message: "Login successful",
             data: await this.authService.login(loginDto)
         }
+    }
+
+    @Post('change-password')
+    @UseGuards(JwtAuthGuard)
+    async changePassword(
+        @CurrentUser() user: AuthenticatedUser,
+        @Body() changePasswordDto: ChangePasswordDto,
+    ) {
+        return await this.authService.changePassword(user.id, changePasswordDto);
     }
 }
